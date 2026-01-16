4d7893018cdf8d2d639bbc79a34bc11bde66e0da
Miguel01Santos <migueldossantos.pro@outlook.com>
2026-01-15 23:21:48 -0300

    feat: configurar orm e criar modulo  de usuarios

--- packages/server/src/resolvers/commom/commom.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserResolver } from '../users';
-
-@Module({
-  imports: [],
-  providers: [UserResolver],
-  exports: [UserResolver],
-})
-export class CommonModule {}
