e77a16729f88408934c8d786672964e22f1ee40a
Miguel01Santos <migueldossantos.pro@outlook.com>
2026-01-30 17:31:18 -0300

    feat: configurar type orm

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -5,26 +5,43 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CommonModule } from './app/resolvers/common.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from 'env-type-key';
+import { join } from 'path';
 
 @Module({
   imports: [
-    CommonModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
+      typePaths: [join(process.cwd(), 'src/app/schemas/**/*.gql')],
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'seu_usuario',
-      password: 'sua_senha',
-      database: 'seu_banco',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => {
+        const host = configService.get<string>('DB_HOST');
+        const port = Number(configService.get('DB_PORT')) || 5433;
+        const username = configService.get<string>('DB_USERNAME');
+        const password = configService.get<string>('DB_PASSWORD');
+        const database = configService.get<string>('DB_NAME');
+
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username,
+          password,
+          database,
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CommonModule],
 })
 export class AppModule {}
